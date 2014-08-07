cf554ddf1b253041bcdce20b9d59de6caf4f9c93
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-08-07 17:31:25 +0530

    allow fullscreen iframes

--- src/textAngular-sanitize.js
+++ src/textAngular-sanitize.js
@@ -214,7 +214,7 @@ var validAttrs = angular.extend({}, uriAttrs, makeMap(
     'color,cols,colspan,compact,coords,dir,face,headers,height,hreflang,hspace,'+
     'ismap,lang,language,nohref,nowrap,rel,rev,rows,rowspan,rules,'+
     'scope,scrolling,shape,size,span,start,summary,target,title,type,'+
-    'valign,value,vspace,width'));
+    'valign,value,vspace,width,allowfullscreen'));
 
 function makeMap(str) {
   var obj = {}, items = str.split(','), i;
